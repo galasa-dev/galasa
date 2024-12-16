25ca25b5f9381de15180ee449310e1fedda5b8d7
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-12-16 11:33:19 +0000

    docs: Add javadoc comment for IpRateLimiter class
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- modules/framework/galasa-parent/dev.galasa.framework.api/src/main/java/dev/galasa/framework/api/internal/IpRateLimiter.java
+++ modules/framework/galasa-parent/dev.galasa.framework.api/src/main/java/dev/galasa/framework/api/internal/IpRateLimiter.java
@@ -10,6 +10,10 @@ import java.time.Instant;
 import dev.galasa.framework.api.IRateLimiter;
 import dev.galasa.framework.spi.utils.ITimeService;
 
+/**
+ * A rate limiter wrapper used for IP-based rate limiting to keep track of the last time
+ * a client sent a request to the API server.
+ */
 public class IpRateLimiter implements IRateLimiter {
     private final IRateLimiter rateLimiter;
     private ITimeService timeService;
