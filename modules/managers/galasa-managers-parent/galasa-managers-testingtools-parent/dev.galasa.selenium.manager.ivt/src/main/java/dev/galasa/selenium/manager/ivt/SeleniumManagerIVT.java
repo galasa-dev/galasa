d1924017e5a11e41894b52df91052c8793d20e18
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-12-31 14:18:30 +0000

    Fix selenium manager IVT failures, add docs on how to run the IVT locally
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- modules/managers/galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.selenium.manager.ivt/src/main/java/dev/galasa/selenium/manager/ivt/SeleniumManagerIVT.java
+++ modules/managers/galasa-managers-parent/galasa-managers-testingtools-parent/dev.galasa.selenium.manager.ivt/src/main/java/dev/galasa/selenium/manager/ivt/SeleniumManagerIVT.java
@@ -6,9 +6,9 @@
 package dev.galasa.selenium.manager.ivt;
 
 import static org.assertj.core.api.Assertions.assertThat;
-// import static org.junit.Assert.assertThat;
 
 import org.apache.commons.logging.Log;
+import org.openqa.selenium.Keys;
 
 import dev.galasa.Test;
 import dev.galasa.core.manager.Logger;
@@ -36,37 +36,37 @@ public class SeleniumManagerIVT {
     @WebDriver (browser = Browser.EDGE)
     public IWebDriver driverEdge;
 
-    public static final String WEBSITE = "https://duckduckgo.com";
-    public static final String WEBSITEGALASAGITHUB = "https://github.com/galasa-dev";
-    public static final String TITLE = "DuckDuckGo";
+    public static final String DUCKDUCKGO_WEBSITE = "https://duckduckgo.com";
+    public static final String WEBSITE_GALASA_GITHUB = "https://github.com/galasa-dev";
+    public static final String DUCKDUCKGO_TITLE = "DuckDuckGo";
     public static final String VALUE = "value";
-    public static final String SEARCHID = "search_form_input_homepage";
+    public static final String DUCKDUCKGO_SEARCH_ID = "searchbox_input";
 
     // Test Broswers support
     @Test
     public void testChromeOptionsCanBeUsed() throws SeleniumManagerException {
     	IChromeOptions options = driverChrome.getChromeOptions();
-        IWebPage page = driverChrome.allocateWebPage(WEBSITE, options);
+        IWebPage page = driverChrome.allocateWebPage(DUCKDUCKGO_WEBSITE, options);
         page.maximize().takeScreenShot();
-        assertThat(page.getTitle()).containsOnlyOnce(TITLE);
+        assertThat(page.getTitle()).containsOnlyOnce(DUCKDUCKGO_TITLE);
         page.quit();
     }
     
     @Test
     public void testFirefoxOptionsCanBeUsed() throws SeleniumManagerException {
     	IFirefoxOptions options = driverFirefox.getFirefoxOptions();
-        IWebPage page = driverFirefox.allocateWebPage(WEBSITE, options);
+        IWebPage page = driverFirefox.allocateWebPage(DUCKDUCKGO_WEBSITE, options);
         page.maximize().takeScreenShot();
-        assertThat(page.getTitle()).containsOnlyOnce(TITLE);
+        assertThat(page.getTitle()).containsOnlyOnce(DUCKDUCKGO_TITLE);
         page.quit();
     }
     
     @Test
     public void testEdgeOptionsCanBeUsed() throws SeleniumManagerException {
     	IEdgeOptions options = driverEdge.getEdgeOptions();
-        IWebPage page = driverEdge.allocateWebPage(WEBSITE, options);
+        IWebPage page = driverEdge.allocateWebPage(DUCKDUCKGO_WEBSITE, options);
         page.maximize().takeScreenShot();
-        assertThat(page.getTitle()).containsOnlyOnce(TITLE);
+        assertThat(page.getTitle()).containsOnlyOnce(DUCKDUCKGO_TITLE);
         page.quit();
     }
     
@@ -74,7 +74,7 @@ public class SeleniumManagerIVT {
     public void testChromeArguments() throws SeleniumManagerException {
     	IChromeOptions options = driverChrome.getChromeOptions();
     	options.addArguments("--ignore-ssl-errors=yes");
-    	IWebPage page = driverChrome.allocateWebPage(WEBSITE, options);
+    	IWebPage page = driverChrome.allocateWebPage(DUCKDUCKGO_WEBSITE, options);
     	page.takeScreenShot();
     	page.quit();
     }
@@ -83,7 +83,7 @@ public class SeleniumManagerIVT {
     public void testFirefoxArguments() throws SeleniumManagerException {
     	IFirefoxOptions options = driverFirefox.getFirefoxOptions();
     	options.addArguments("--ignore-ssl-errors=yes");
-    	IWebPage page = driverFirefox.allocateWebPage(WEBSITE, options);
+    	IWebPage page = driverFirefox.allocateWebPage(DUCKDUCKGO_WEBSITE, options);
     	page.takeScreenShot();
     	page.quit();
     }
@@ -91,46 +91,45 @@ public class SeleniumManagerIVT {
     // Some basic Tests
     @Test
     public void sendingKeysAndClearingFields() throws SeleniumManagerException {
-        IWebPage page = driverFirefox.allocateWebPage(WEBSITE);
+        IWebPage page = driverFirefox.allocateWebPage(DUCKDUCKGO_WEBSITE);
         page.maximize().takeScreenShot();
-        assertThat(page.getTitle()).containsOnlyOnce(TITLE);
-        assertThat(page.findElementById(SEARCHID).getAttribute(VALUE)).isEmpty();
-        page.sendKeysToElementById(SEARCHID, "galasa");
-        assertThat(page.findElementById(SEARCHID).getAttribute(VALUE)).isEqualTo("galasa");
-        page.clearElementById(SEARCHID);
-        assertThat(page.findElementById(SEARCHID).getAttribute(VALUE)).isEmpty();
+        assertThat(page.getTitle()).containsOnlyOnce(DUCKDUCKGO_TITLE);
+        assertThat(page.findElementById(DUCKDUCKGO_SEARCH_ID).getDomAttribute(VALUE)).isEmpty();
+        page.sendKeysToElementById(DUCKDUCKGO_SEARCH_ID, "galasa");
+        assertThat(page.findElementById(DUCKDUCKGO_SEARCH_ID).getDomAttribute(VALUE)).isEqualTo("galasa");
+        page.clearElementById(DUCKDUCKGO_SEARCH_ID);
+        assertThat(page.findElementById(DUCKDUCKGO_SEARCH_ID).getDomAttribute(VALUE)).isEmpty();
         page.quit();
     }
    
 
     @Test
     public void clickingFields() throws SeleniumManagerException {
-        IWebPage page = driverFirefox.allocateWebPage(WEBSITE);
+        IWebPage page = driverFirefox.allocateWebPage(WEBSITE_GALASA_GITHUB);
         page.maximize().takeScreenShot();
-        assertThat(page.getTitle()).containsOnlyOnce(TITLE);
-        page.clickElementByCssSelector("a.header__button--menu.js-side-menu-open")
-            .clickElementByLinkText("Twitter").takeScreenShot()
-            .waitForElementByLinkText("duckduckgo.com");
-        assertThat(page.getTitle()).contains("DuckDuckGo (@DuckDuckGo)");
+        assertThat(page.getTitle()).containsOnlyOnce("galasa");
+        page.sendKeysToElementByCssSelector(".auto-search-input", "galasa")
+            .clickElementByLinkText("galasa").takeScreenShot()
+            .waitForElementByLinkText("modules");
+        assertThat(page.getTitle()).contains("The Galasa source code");
         page.quit();
-    }   
+    }
 
     @Test
     public void navigateGalasaGithub() throws SeleniumManagerException {
-        IWebPage page = driverFirefox.allocateWebPage(WEBSITE);
+        IWebPage page = driverFirefox.allocateWebPage(DUCKDUCKGO_WEBSITE);
         page.maximize().takeScreenShot();
-        assertThat(page.getTitle()).containsOnlyOnce(TITLE);
-        page.takeScreenShot().sendKeysToElementById(SEARCHID, "galasa dev github").takeScreenShot()
-            .clickElementById("search_button_homepage").takeScreenShot()
-            .clickElementByLinkText("galasa - GitHub").takeScreenShot()
-            .clickElementByPartialLinkText("Repositories").takeScreenShot();
-        assertThat(page.findElementsByLinkText("framework")).isNotEmpty();
+        assertThat(page.getTitle()).containsOnlyOnce(DUCKDUCKGO_TITLE);
+        page.takeScreenShot().sendKeysToElementById(DUCKDUCKGO_SEARCH_ID, "galasa dev github").takeScreenShot()
+            .sendKeysToElementById(DUCKDUCKGO_SEARCH_ID, Keys.RETURN).takeScreenShot()
+            .clickElementByPartialLinkText("The Galasa source code").takeScreenShot();
+        assertThat(page.findElementsByLinkText("tools")).isNotEmpty();
         page.quit();
     }
     
     @Test
     public void testNotCleaningUp() throws SeleniumManagerException {
-        IWebPage page = driverChrome.allocateWebPage(WEBSITE);
+        IWebPage page = driverChrome.allocateWebPage(DUCKDUCKGO_WEBSITE);
         page.takeScreenShot();
         page.maximize();
     }
