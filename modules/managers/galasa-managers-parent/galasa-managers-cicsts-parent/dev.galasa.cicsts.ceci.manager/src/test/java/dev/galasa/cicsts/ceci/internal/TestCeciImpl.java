7978aa9766d4beb834f49fb60705c25e0a621a59
Ian Deeley <ian_deeley@uk.ibm.com>
2025-02-18 18:44:42 +0000

    More CECI test mocks that need to be lenient with uplifted Mockito
    Signed-off-by: Ian Deeley <ian_deeley@uk.ibm.com>

--- modules/managers/galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.ceci.manager/src/test/java/dev/galasa/cicsts/ceci/internal/TestCeciImpl.java
+++ modules/managers/galasa-managers-parent/galasa-managers-cicsts-parent/dev.galasa.cicsts.ceci.manager/src/test/java/dev/galasa/cicsts/ceci/internal/TestCeciImpl.java
@@ -72,21 +72,21 @@ public class TestCeciImpl {
         ceciSpy = Mockito.spy(ceci);
 
         // Mock all terminal function
-        Mockito.when(ceciTerminalMock.type(Mockito.any())).thenReturn(ceciTerminalMock);
+        Mockito.lenient().when(ceciTerminalMock.type(Mockito.any())).thenReturn(ceciTerminalMock);
         Mockito.when(ceciTerminalMock.enter()).thenReturn(ceciTerminalMock);
-        Mockito.when(ceciTerminalMock.pf2()).thenReturn(ceciTerminalMock);
+        Mockito.lenient().when(ceciTerminalMock.pf2()).thenReturn(ceciTerminalMock);
         Mockito.lenient().when(ceciTerminalMock.pf3()).thenReturn(ceciTerminalMock);
-        Mockito.when(ceciTerminalMock.pf4()).thenReturn(ceciTerminalMock);
-        Mockito.when(ceciTerminalMock.pf5()).thenReturn(ceciTerminalMock);
-        Mockito.when(ceciTerminalMock.pf9()).thenReturn(ceciTerminalMock);
-        Mockito.when(ceciTerminalMock.pf10()).thenReturn(ceciTerminalMock);
+        Mockito.lenient().when(ceciTerminalMock.pf4()).thenReturn(ceciTerminalMock);
+        Mockito.lenient().when(ceciTerminalMock.pf5()).thenReturn(ceciTerminalMock);
+        Mockito.lenient().when(ceciTerminalMock.pf9()).thenReturn(ceciTerminalMock);
+        Mockito.lenient().when(ceciTerminalMock.pf10()).thenReturn(ceciTerminalMock);
         Mockito.when(ceciTerminalMock.pf11()).thenReturn(ceciTerminalMock);
         Mockito.when(ceciTerminalMock.tab()).thenReturn(ceciTerminalMock);
-        Mockito.when(ceciTerminalMock.home()).thenReturn(ceciTerminalMock);
+        Mockito.lenient().when(ceciTerminalMock.home()).thenReturn(ceciTerminalMock);
         Mockito.when(ceciTerminalMock.newLine()).thenReturn(ceciTerminalMock);
         Mockito.when(ceciTerminalMock.eraseEof()).thenReturn(ceciTerminalMock);
         Mockito.when(ceciTerminalMock.waitForKeyboard()).thenReturn(ceciTerminalMock);
-        Mockito.when(ceciTerminalMock.reportScreenWithCursor()).thenReturn(ceciTerminalMock);
+        Mockito.lenient().when(ceciTerminalMock.reportScreenWithCursor()).thenReturn(ceciTerminalMock);
         Mockito.when(ceciTerminalMock.getCicsRegion()).thenReturn(cicsRegionMock);
         Mockito.when(wrongCeciTerminalMock.getCicsRegion()).thenReturn(wrongCicsRegionMock);
     }
