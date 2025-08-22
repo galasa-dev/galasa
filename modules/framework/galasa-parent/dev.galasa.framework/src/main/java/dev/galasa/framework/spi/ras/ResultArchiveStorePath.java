e87d3b6dcf75884e0f991de35ceca791021224ea
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2025-08-22 10:22:00 +0100

    fix: Encode spaces in RAS paths to stop an illegal character exception from being thrown, add tests for paths with spaces
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- modules/framework/galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/spi/ras/ResultArchiveStorePath.java
+++ modules/framework/galasa-parent/dev.galasa.framework/src/main/java/dev/galasa/framework/spi/ras/ResultArchiveStorePath.java
@@ -58,6 +58,10 @@ public class ResultArchiveStorePath implements Path {
             path = path.replaceAll("\\Q\\\\E", "/"); // NOSONAR
         }
 
+        if (path.contains(" ")) {
+            path = path.replaceAll(" ", "%20");
+        }
+
         this.absolute = path.startsWith("/");
         if (path.endsWith("/")) {
             path = path.substring(0, path.length() - 1);
