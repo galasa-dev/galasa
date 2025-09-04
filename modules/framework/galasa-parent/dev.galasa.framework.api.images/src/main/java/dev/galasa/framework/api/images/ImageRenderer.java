7173c1f990df64d1ffac884e1b169ae9eb553352
techcobweb <77053+techcobweb@users.noreply.github.com>
2025-09-04 09:05:13 +0100

    moved terminal image classes into framework from manager module
    Signed-off-by: techcobweb <77053+techcobweb@users.noreply.github.com>

--- modules/framework/galasa-parent/dev.galasa.framework.api.images/src/main/java/dev/galasa/framework/api/images/ImageRenderer.java
+++ modules/framework/galasa-parent/dev.galasa.framework.api.images/src/main/java/dev/galasa/framework/api/images/ImageRenderer.java
@@ -1,57 +1,52 @@
-
+/*
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package dev.galasa.framework.api.images;
 
-import java.awt.Color;
-import java.awt.Font;
+// import java.awt.Color;
+// import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
 import javax.imageio.ImageIO;
+
+import dev.galasa.zos3270.common.screens.TerminalImage;
 
 public class ImageRenderer {
 
-    public void myFunction() throws Exception {
-        throw new Exception("It failed!");
+    public byte[]  render(
+        TerminalImage imageDescriptionToRender, 
+        ImageFormat desiredFormat
+    ) throws Exception {
+
+        // imageDescriptionToRender.getImageSize();
+
+        BufferedImage image = new BufferedImage(560, 351,
+        BufferedImage.TYPE_INT_ARGB);
+        Graphics2D canvas = image.createGraphics();
+
+        // Paint the entire image with a background color.
+        canvas.setColor(ImageColor.BLACK.getRGBColor());
+        canvas.fillRect(0, 0, image.getWidth(), image.getHeight());
+
+        // Set font and text
+        // int fontSize = 12;
+        // canvas.setFont(new Font("Monospaced", Font.PLAIN, fontSize ));
+        // canvas.setColor(ImageColor.GREEN.getRGBColor());
+
+        // int x = 0;
+        // int y = 0;
+        // canvas.drawString("Hello, World!", x,y );
+
+        // Save the graphics as a byte array
+        ByteArrayOutputStream renderedImage = new ByteArrayOutputStream();
+        ImageIO.write(image, desiredFormat.getValue(), renderedImage);
+        renderedImage.close();
+
+        byte[] imageBytes = renderedImage.toByteArray();
+
+        return imageBytes;
     }
-
-    // public static void main(String[] args) {
-    // try {
-    // BufferedImage image = new BufferedImage(560, 351,
-    // BufferedImage.TYPE_INT_ARGB);
-    // Graphics2D g2d = image.createGraphics();
-    // g2d.setColor(new Color(0, 0, 0));
-    // g2d.fillRect(0, 0, image.getWidth(), image.getHeight());
-
-    // // Set font and text
-    // int fontSize = 12;
-    // g2d.setFont(new Font("Monospaced", Font.PLAIN, fontSize ));
-    // g2d.setColor(new Color(0, 255, 0));
-    // g2d.drawString("Hello, World! Hello, World! Hello, World! Hello, World!
-    // Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello,
-    // World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World!
-    // Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello,
-    // World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World!
-    // Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello,
-    // World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World!
-    // Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello,
-    // World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World!
-    // Hello, World! Hello, World! Hello, World! Hello, World! Hello, World! Hello,
-    // World! Hello, World! Hello, World! Hello, World! Hello, World! Hello, World!
-    // Hello, World! ", 0, fontSize);
-
-    // // Save the image
-    // File imageFile = new File("black_image_with_green_text.png");
-    // OutputStream out = new FileOutputStream(imageFile);
-    // ImageIO.write(image, "png", out);
-    // out.close();
-
-    // System.out.println("Image saved as 'black_image_with_green_text.png'");
-
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-    // }
 }
--- modules/framework/galasa-parent/dev.galasa.framework.api.images/src/test/java/dev/galasa/framework/api/images/TestImageRenderer.java
+++ modules/framework/galasa-parent/dev.galasa.framework.api.images/src/test/java/dev/galasa/framework/api/images/TestImageRenderer.java
@@ -1,20 +1,142 @@
+/*
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package dev.galasa.framework.api.images;
 
-import java.time.Instant;
-import static javax.servlet.http.HttpServletResponse.*;
-import java.util.*;
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
-
+import dev.galasa.zos3270.common.screens.FieldContents;
+import dev.galasa.zos3270.common.screens.TerminalField;
+import dev.galasa.zos3270.common.screens.TerminalImage;
+import dev.galasa.zos3270.common.screens.TerminalSize;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class TestImageRenderer {
 
+  @Rule
+  public TestName testName = new TestName();
+
+  private void saveImage(String fileNameUniquePart, ImageFormat format, byte[] imageBytes) throws IOException {
+    // Make sure the images folder exists.
+    String folderName = "images/"+this.getClass().getSimpleName();
+    new File(folderName).mkdirs();
+
+    // Dump the binary data into the file
+    String fileName = folderName + "/" + testName.getMethodName() + "-" + fileNameUniquePart + "."+format.getValue();
+    try (FileOutputStream out = new FileOutputStream(fileName)) {
+      out.write(imageBytes);
+    }
+  }
+
   @Test
-  public void testSomething() throws Exception {
+  public void testCanCreateABlankImage() throws Exception {
+
+    // Given
     ImageRenderer renderer = new ImageRenderer();
-    renderer.myFunction();
+
+    int sequence = 23;
+    String id = "imageId123";
+    boolean isInbound = false;
+    String interactionType = "interactionType";
+    String attentionIdForOutboundMessages = "Hi";
+    int cursorColumn = 10 ;
+    int cursorRow = 4;
+
+    int columns = 80;
+    int rows = 24;
+    TerminalSize imageSize = new TerminalSize(columns,rows);
+
+    TerminalImage image = new TerminalImage( 
+            sequence,  id, isInbound, interactionType, attentionIdForOutboundMessages,
+            imageSize, cursorColumn, cursorRow);
+
+    ImageFormat desiredFormat = ImageFormat.PNG;
+
+    // When
+    byte[] imageBytes = renderer.render(image, desiredFormat);
+
+    // Then
+    assertThat(imageBytes).isNotNull().isNotEmpty();
+    assertThat(imageBytes.length).isGreaterThan(40);
+
+    // So we can view the image manually... save it to disk.
+    saveImage("1", ImageFormat.PNG, imageBytes);
+  }
+
+
+  @Test
+  public void testCanCreateABlackPageWithHelloTopLeft() throws Exception {
+
+    // Given
+    ImageRenderer renderer = new ImageRenderer();
+
+    int sequence = 23;
+    String id = "imageId123";
+    boolean isInbound = false;
+    String interactionType = "interactionType";
+    String attentionIdForOutboundMessages = "Hi";
+    int cursorColumn = 10 ;
+    int cursorRow = 4;
+
+    int columns = 80;
+    int rows = 24;
+    TerminalSize imageSize = new TerminalSize(columns,rows);
+
+    TerminalImage image = new TerminalImage( 
+            sequence,  id, isInbound, interactionType, attentionIdForOutboundMessages,
+            imageSize, cursorColumn, cursorRow);
+
+    // Add a text field top-left
+    int row = 0;
+    int column= 0; 
+    boolean isUnFormatted = false;
+    boolean isFieldProtected = false;
+    boolean isFieldNumeric = false;
+    boolean isFieldDisplayed = true;
+    boolean isFieldIntenseDisplay = false;
+    boolean isFieldSelectorPen = false;
+    boolean isFieldModified = false;
+    Character foregroundColour = ImageColor.GREEN.getShortName();
+    Character backgroundColour = ImageColor.BLACK.getShortName();
+    Character highlight = 'a';
+
+    // Create a field to contain some text
+    TerminalField field = new TerminalField(row,column,isUnFormatted,isFieldProtected,isFieldNumeric, 
+    isFieldDisplayed, isFieldIntenseDisplay, isFieldSelectorPen, isFieldModified, 
+    foregroundColour , backgroundColour, highlight);
+
+    // Add some text to the field
+    String message = "Hello";
+    char[] chars = message.toCharArray();
+    Character[] characters = new Character[chars.length];
+    int i = 0 ;
+    for(i = 0 ; i<chars.length; i++) {
+      characters[i] = chars[i];
+    }
+    FieldContents fieldContents = new FieldContents(characters);
+    field.getContents().add(fieldContents);
+
+    // Add the field to the image
+    image.getFields().add(field);
+
+    ImageFormat desiredFormat = ImageFormat.PNG;
+
+    // When
+    byte[] imageBytes = renderer.render(image, desiredFormat);
+
+    // Then
+    assertThat(imageBytes).isNotNull().isNotEmpty();
+    assertThat(imageBytes.length).isGreaterThan(40);
+
+    // So we can view the image manually... save it to disk.
+    saveImage("1", ImageFormat.PNG, imageBytes);
   }
 }
